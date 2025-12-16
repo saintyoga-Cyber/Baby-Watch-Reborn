3e4a6177554feee090456f190cfaf575bf623ee9
saintyoga-Cyber <saintyoga@gmail.com>
2025-12-16 12:43:33 -0800

    Simplify timeline to Rebble-only (remove backend config options)

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,48 +1,24 @@
-// Timeline API Configuration
-// Change this to switch between backends:
-// - 'rebble'  : Rebble services (current, works with old Pebble app)
-// - 'core'    : Core Devices (for new Core 2 watches - update URL when available)
-// - 'custom'  : Use CUSTOM_API_URL below
-var TIMELINE_BACKEND = 'rebble';
+// Timeline API - Rebble Services
+// Uses Rebble timeline API for pin delivery
 
-// Backend URLs
-var BACKEND_URLS = {
-  'rebble': 'https://timeline-api.rebble.io/',
-  'core': 'https://timeline-api.rebble.io/',  // Update when Core Devices releases their API
-  'custom': ''  // Set your custom URL here if needed
-};
-
-// Custom API URL (only used if TIMELINE_BACKEND = 'custom')
-var CUSTOM_API_URL = '';
-
-// Get the active API URL
-function getApiUrl() {
-  if (TIMELINE_BACKEND === 'custom' && CUSTOM_API_URL) {
-    return CUSTOM_API_URL;
-  }
-  return BACKEND_URLS[TIMELINE_BACKEND] || BACKEND_URLS['rebble'];
-}
+var TIMELINE_API_URL = 'https://timeline-api.rebble.io/';
 
 /**
- * Send a request to the Pebble public web timeline API.
+ * Send a request to the Rebble timeline API.
  * @param pin The JSON pin to insert. Must contain 'id' field.
  * @param type The type of request, either PUT or DELETE.
- * @param topics Array of topics if a shared pin, 'null' otherwise.
- * @param apiKey Timeline API key for this app, available from dev-portal.getpebble.com
  * @param callback The callback to receive the responseText after the request has completed.
  */
-function timelineRequest(pin, type, topics, apiKey, callback) {
-  var apiUrl = getApiUrl();
-  
-  // User or shared?
-  var url = apiUrl + 'v1/' + ((topics != null) ? 'shared/' : 'user/') + 'pins/' + pin.id;
+function timelineRequest(pin, type, callback) {
+  var url = TIMELINE_API_URL + 'v1/user/pins/' + pin.id;
 
-  console.log('timeline: using backend: ' + TIMELINE_BACKEND + ' (' + apiUrl + ')');
+  console.log('timeline: sending ' + type + ' request to Rebble API');
+  console.log('timeline: URL: ' + url);
 
-  // Create XHR
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    console.log('timeline: response received: ' + this.responseText);
+    console.log('timeline: response status: ' + this.status);
+    console.log('timeline: response: ' + this.responseText);
     callback(this.responseText);
   };
   xhr.onerror = function() {
@@ -50,24 +26,16 @@ function timelineRequest(pin, type, topics, apiKey, callback) {
     callback('{"error": "Request failed"}');
   };
   xhr.open(type, url);
-
-  // Set headers
   xhr.setRequestHeader('Content-Type', 'application/json');
-  if(topics != null) {
-    xhr.setRequestHeader('X-Pin-Topics', '' + topics.join(','));
-    xhr.setRequestHeader('X-API-Key', '' + apiKey);
-  }
 
-  // Get token
+  console.log('timeline: getting timeline token...');
   Pebble.getTimelineToken(function(token) {
-    // Add headers
+    console.log('timeline: token acquired: ' + token.substring(0, 10) + '...');
     xhr.setRequestHeader('X-User-Token', '' + token);
-
-    // Send
     xhr.send(JSON.stringify(pin));
-    console.log('timeline: request sent to ' + url);
+    console.log('timeline: request sent');
   }, function(error) { 
-    console.log('timeline: error getting timeline token: ' + error); 
+    console.log('timeline: ERROR getting token: ' + error); 
     callback('{"error": "Failed to get timeline token: ' + error + '"}');
   });
 }
@@ -78,7 +46,7 @@ function timelineRequest(pin, type, topics, apiKey, callback) {
  * @param callback The callback to receive the responseText after the request has completed.
  */
 function insertUserPin(pin, callback) {
-  timelineRequest(pin, 'PUT', null, null, callback);
+  timelineRequest(pin, 'PUT', callback);
 }
 
 /**
@@ -87,17 +55,8 @@ function insertUserPin(pin, callback) {
  * @param callback The callback to receive the responseText after the request has completed.
  */
 function deleteUserPin(pin, callback) {
-  timelineRequest(pin, 'DELETE', null, null, callback);
+  timelineRequest(pin, 'DELETE', callback);
 }
 
-/**
- * Get the current backend name
- */
-function getBackendName() {
-  return TIMELINE_BACKEND;
-}
-
-// Export
 module.exports.insertUserPin = insertUserPin;
 module.exports.deleteUserPin = deleteUserPin;
-module.exports.getBackendName = getBackendName;
